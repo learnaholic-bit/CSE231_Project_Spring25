ea56bc6bf27fabcb38a6b5e4c250fc238bacb618
Andro <Andro_milad22@yahoo.com>
2025-05-13 12:20:59 +0300

    pharmacy item data filed is added

--- src/main/java/org/project/pharmacy/gui/MoreInfoSceneCreator.java
+++ src/main/java/org/project/pharmacy/gui/MoreInfoSceneCreator.java
@@ -5,15 +5,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import org.project.pharmacy.logic.*;
 
-import javax.swing.*;
+
 import java.time.LocalDate;
 
 public class MoreInfoSceneCreator implements SceneProvider {
 
     private MainApp mainApp;
     private Scene scene;
+    private  PharmacyItem pharmacyItemData;
     String addedQuantityString ="0";
     int addedQuantity =0;
     Label quantityLable = new Label();
@@ -23,7 +25,13 @@ public class MoreInfoSceneCreator implements SceneProvider {
     }
     @Override
     public Scene getScene() {
-      return scene;
+        try {
+            return getScene(pharmacyItemData);
+        }
+        catch (NullPointerException e)
+        {
+            return null;
+        }
     }
 
     public Scene getScene(PharmacyItem pharmacyItem) {
@@ -196,5 +204,13 @@ public class MoreInfoSceneCreator implements SceneProvider {
             quantityField.setText("0");
         }
 
+    }
+    void setPharmacyItem(PharmacyItem pharmacyItem)
+    {
+        this.pharmacyItemData = pharmacyItem;
+    }
+    PharmacyItem getPharmacyItem ()
+    {
+        return this.pharmacyItemData;
     }
 }
